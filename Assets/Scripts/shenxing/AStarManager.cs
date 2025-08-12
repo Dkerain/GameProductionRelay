f39c6f7728f8e8e10d29dbc0de3bc0334b14da09
shenxing <153624036+tangjinyao@users.noreply.github.com>
2025-08-12 23:25:41 +0800

    实现了敌人向玩家移动

---
+++ Assets/Scripts/shenxing/AStarManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敌人的寻路
+/// 参考场景：AStarTestScene.unity
+/// 插件参考视频：https://www.bilibili.com/video/BV1X5411k7uk
+/// 实现了敌人一直通过A*算法寻路向主角逼近
+/// （本脚本就是个说明书，没啥实际功能）
+/// </summary>
+
+public class AStarManager : MonoBehaviour
+{
+
+}
